f16d759ef67a8af076782d2d7cf148edc399a5fa
Brayden Corbridge <bbcorbridge@gmail.com>
2026-02-03 10:37:48 -0700

    Starting code for class

--- semesterLongProject/JobBoardMulitPage-React/src/pages/Profile.jsx
+++ semesterLongProject/JobBoardMulitPage-React/src/pages/Profile.jsx
@@ -3,72 +3,66 @@ import { useRef, useState } from "react";
 
 let counter = 0;
 const Profile = () => {
-  const fullNameRef = useRef();
-  const emailRef = useRef();
-  const titleRef = useRef();
-  const bioRef = useRef();
 
-  const [profiles, setProfiles] = useState([]);
-  console.log("Rendering for the " + ++counter + " time");
+    const fullNameRef = useRef();
+    const emailRef = useRef();
+    const titleRef = useRef();
+    const bioRef = useRef();
+    const [history,setHistory] = useState([]);
+    
+    const updateHistory = ()=>{
+      setHistory(old=>([...old,{
+        fullName:fullNameRef.current.value,
+        email:emailRef.current.value,
+        title:titleRef.current.value,
+        bio:bioRef.current.value,
+      }]));
 
-  function handleClick() {
-    setProfiles((old) => [
-      ...old,
-      {
-        fullName: fullNameRef.current.value,
-        email: emailRef.current.value,
-        title: titleRef.current.value,
-        bio: bioRef.current.value,
-      },
-    ]);
+      fullNameRef.current.value = "";
+      emailRef.current.value = "";
+      titleRef.current.value = "";
+      bioRef.current.value = "";
 
-    // Clear the input fields
-    fullNameRef.current.value = "";
-    emailRef.current.value = "";
-    titleRef.current.value = "";
-    bioRef.current.value = "";
-
-    // Focus on the first field
-    fullNameRef.current.focus();
-  }
-
+      fullNameRef.current.focus()
+    }
+    
+    console.log("Rendering for the "+ ++counter + " time")
   return (
+    <>
     <Grid container spacing={2}>
       <Grid size={12}>
         <h2>My Profile</h2>
       </Grid>
       <Grid size={12}>
-        <TextField label="Full Name" fullWidth inputRef={fullNameRef} />
+      <TextField label="Full Name" fullWidth inputRef={fullNameRef}/>
+
       </Grid>
       <Grid size={6}>
-        <TextField label="Email" fullWidth inputRef={emailRef} />
+      <TextField label="Email" fullWidth inputRef={emailRef}/>
+
       </Grid>
       <Grid size={6}>
-        <TextField label="Current Title" fullWidth inputRef={titleRef} />
+      <TextField label="Current Title" fullWidth inputRef={titleRef}/>
       </Grid>
       <Grid size={12}>
-        <TextField label="Bio/Summary" multiline fullWidth inputRef={bioRef} />
+        <TextField label="Bio/Summary" multiline fullWidth inputRef={bioRef}/>
       </Grid>
       <Grid container>
-        <Button variant="contained" size="large" onClick={handleClick}>
-          Save
-        </Button>
-        <Button variant="outlined" size="large">
-          Cancel
-        </Button>
+        <Button variant="contained" size="large" onClick={updateHistory}>Save</Button>
+        <Button variant="outlined" size="large">Cancel</Button>
       </Grid>
       <Grid container size={12}>
-        {profiles.map((item) => (
-          <Grid size={4} key={item.title}>
-            <h1>{item.fullName}</h1>
-            <h4>
-              {item.title} - {item.email}
-            </h4>
-            <p>{item.bio}</p>
-          </Grid>
+        {history.map(item=>(
+        <Grid size={4} key={item.title}>
+          <h1>{item.fullName}</h1>
+          <h4>{item.title} - {item.email}</h4>
+          <p>{item.bio}</p>
+        </Grid>
         ))}
+
       </Grid>
     </Grid>
+      </>
   );
 };
 
