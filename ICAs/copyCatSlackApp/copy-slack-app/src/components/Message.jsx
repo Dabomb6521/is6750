075a77354ae71cf91519658b2a06b4a9fc199c5f
Brayden Corbridge <bbcorbridge@gmail.com>
2026-02-16 13:06:17 -0700

    uncommitted changes from class

--- ICAs/copyCatSlackApp/copy-slack-app/src/components/Message.jsx
+++ ICAs/copyCatSlackApp/copy-slack-app/src/components/Message.jsx
@@ -1,21 +1,26 @@
 import { Paper, Typography } from "@mui/material";
+import { useContext } from "react";
+import { MessagesContext } from "../store/MessagesStore";
 
 const Message = ({ msg }) => {
   // TO DO: Set this up to use the current profile. Right now it is hard coded to User 1
+  const { users, currentUser } = useContext(MessagesContext);
+
+  const sender = users.find((user) => user.id === msg.senderId);
+  const isCurrentUser = msg.senderId === currentUser.id;
 
   return (
     <Paper
       sx={{
         p: 1,
         maxWidth: "60%",
-        alignSelf: msg.senderId === "1" ? "flex-end" : "flex-start",
-        bgcolor: msg.senderId === "1" ? "primary.main" : "grey.300",
-        color: msg.senderId === "1" ? "white" : "black",
+        alignSelf: isCurrentUser ? "flex-end" : "flex-start",
+        bgcolor: isCurrentUser ? "primary.main" : "grey.300",
+        color: isCurrentUser ? "white" : "black",
       }}
     >
       <Typography variant="body1">{msg.content}</Typography>
-      {/* TO DO: Replace with User's name */}
-      <Typography variant="subtitle1">{msg.senderId}</Typography>
+      <Typography variant="subtitle1">- {sender?.name ?? "Unkown"}</Typography>
     </Paper>
   );
 };
--- ICAs/copyCatSlackApp/copy-slack-app/src/store/MessagesStore.jsx
+++ ICAs/copyCatSlackApp/copy-slack-app/src/store/MessagesStore.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { getAllMessages, getChannels } from "../utils/slackUtils";
+import { getAllMessages, getChannels, getAllUsers } from "../utils/slackUtils";
 
 // Set up context and give some default values
 export const MessagesContext = createContext({
@@ -8,6 +8,9 @@ export const MessagesContext = createContext({
   setCurrentChannel: (id) => {},
   channels: [],
   setChannels: () => {},
+  users: [],
+  currentUser: "",
+  setCurrentUser: (id) => {},
 });
 
 // Create the provider component that will manage the states and pass down the values
@@ -15,6 +18,8 @@ const MessagesProvider = ({ children }) => {
   const [messages, setMessages] = useState([]);
   const [currentChannel, setCurrentChannel] = useState("1");
   const [channels, setChannels] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState("1");
 
   //   Grab the channels when the app first loads and never again
   useEffect(() => {
@@ -26,6 +31,15 @@ const MessagesProvider = ({ children }) => {
     startChannelFetch();
   }, []);
 
+  useEffect(()=>{
+    const startUserFetch = async () => {
+      const data = await getAllUsers();
+
+      setUsers(data);
+    }
+    startUserFetch();
+  }, []);
+
   //display only the messages for the current channel
   const getMessagesByChannel = (id) => {
     return messages.filter((msg) => msg.channelId === id);
@@ -33,19 +47,15 @@ const MessagesProvider = ({ children }) => {
 
   //   Set up an interval to get all messages. Should run ever 5 seconds. When component unmounts, clear the interval
   useEffect(() => {
-
-
     const interval = setInterval(async () => {
       const data = await getAllMessages();
       setMessages(data);
     }, 5000);
 
-
     return () => clearInterval(interval);
   }, []);
 
   return (
-
     // Wrap children components in provider and provide necessary data
     <MessagesContext
       value={{
@@ -53,6 +63,9 @@ const MessagesProvider = ({ children }) => {
         currentChannel,
         setCurrentChannel,
         channels,
+        users,
+        currentUser,
+        setCurrentUser,
       }}
     >
       {children}
