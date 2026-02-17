c11f4ab21bda02f4053890bdc987b6b46b076693
Brayden Corbridge <bbcorbridge@gmail.com>
2026-02-16 22:03:04 -0700

    end of week7 assignment1

---
+++ modules/week7/assignment1/src/http.js
@@ -0,0 +1,40 @@
+const BACKEND_URL = "http://localhost:3000";
+
+export async function fetchAvailablePlaces() {
+  const response = await fetch(`${BACKEND_URL}/places`);
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch places");
+  }
+
+  return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch(`${BACKEND_URL}/user-places`, {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data");
+  }
+  return resData.message;
+}
+
+export async function fetchUserPlaces() {
+  const response = await fetch(`${BACKEND_URL}/user-places`);
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places");
+  }
+
+  return resData.places;
+}
