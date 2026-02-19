2dfeaeab085864da0009c2678efb05003a259839
Brayden Corbridge <brayden.corbridge@sdl.usu.edu>
2026-02-19 09:51:27 -0700

    End of week7 assignment 2

---
+++ modules/week7/assignment2/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(fetchFn, initialValue) {
+  const [isFetching, setisFetching] = useState()
+  const [error, isError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+
+  useEffect(() => {
+      async function fetchData() {
+        setIsFetching(true);
+        try {
+          const data = await fetchFn();
+          setUserPlaces(data);
+        } catch (error) {
+          setError({ message: error.message || 'Failed to fetch data.' });
+        }
+  
+        setIsFetching(false);
+      }
+  
+      fetchData();
+    }, [fetchFn]);
+
+    return {
+      isFetching,
+      fetchedData,
+      setFetchedData,
+      error
+    }
+}
