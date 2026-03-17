d9e0eed758da956ca08793f073099ebc875164ed
Brayden Corbridge <bbcorbridge@gmail.com>
2026-03-16 21:36:43 -0600

    week11 assignment1 finished

--- modules/week11/assignment1/src/App.js
+++ modules/week11/assignment1/src/App.js
@@ -1,5 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetail from "./pages/ProductDetail";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </Route>
+// )
+
+const router = createBrowserRouter([
+  { 
+    path: "/", 
+    element: <RootLayout />, 
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: 'products/:productId', element: <ProductDetail />}
+    ] 
+  },
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
