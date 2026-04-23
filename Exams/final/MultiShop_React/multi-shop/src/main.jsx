6be8b3b18e06f741cb74f15a09bb7aacc2fda757
Brayden Corbridge <bbcorbridge@gmail.com>
2026-04-22 22:02:59 -0600

    finished with question 8

--- Exams/final/MultiShop_React/multi-shop/src/main.jsx
+++ Exams/final/MultiShop_React/multi-shop/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 import store from "./store/store";
+import { CartContextProvider } from "./store/cart-context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </Provider>
   </StrictMode>,
 );
---
+++ Exams/final/MultiShop_React/multi-shop/src/store/cart-context.jsx
@@ -0,0 +1,85 @@
+import { createContext, useEffect, useState } from "react";
+
+const CartContext = createContext({
+  items: [],
+  numItems: 0,
+  subtotal: 0,
+  shipping: 0,
+  total: 0,
+  addItem: (item) => {},
+  removeItem: (itemId) => {},
+  updatedItemQuantity: (itemId, newQuantity) => {},
+  resetCart: () => {},
+});
+
+export const CartContextProvider = ({ children }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const cachedCart = sessionStorage.getItem("cartItems");
+    if (cachedCart) {
+      setItems(JSON.parse(cachedCart));
+    }
+  }, []);
+
+  const numItems = items.length;
+  const subtotal = items.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
+  const shipping = subtotal * 0.1;
+  const total = subtotal + shipping;
+
+  const addItem = (item) => {
+    setItems((prev) => {
+      const existing = prev.find((i) => i.id === item.id);
+      let updated;
+      if (existing) {
+        updated = prev.map((i) =>
+          i.id === item.id ? { ...i, quantity: i.quantity + item.quantity } : 1,
+        );
+      } else {
+        updated = [...prev, item];
+      }
+      sessionStorage.setItem("cartItems", JSON.stringify(updated));
+      return updated;
+    });
+  };
+
+  const removeItem = (itemId) => {
+    setItems((prev) => {
+      const updated = prev.filter((i) => i.id !== itemId);
+      sessionStorage.setItem("cartItems", JSON.stringify(updated));
+      return updated;
+    });
+  };
+
+  const updateItemQuantity = (itemId, newQuantity) => {
+    setItems((prev) => {
+      const updated = prev.map((i) =>
+        i.id === itemId ? { ...1, quantity: newQuantity } : 1,
+      );
+      sessionStorage.setItem("cartItems", JSON.stringify(updated));
+      return updated;
+    });
+  };
+
+  const resetCart = () => {
+    setItems([]);
+    sessionStorage.removeItem("cartItem");
+  };
+
+  const contextValue = {
+    items,
+    numItems,
+    subtotal,
+    shipping,
+    total,
+    addItem,
+    removeItem,
+    updateItemQuantity,
+    resetCart,
+  };
+
+  return <CartContext value={contextValue}>{children}</CartContext>;
+};
